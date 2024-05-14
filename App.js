a70631ebc1684140a73b5d660d2e0f483f1c81cd
iKataria <17ktaria@gmail.com>
2024-05-14 12:28:23 -0400

    Episode 1 done

---
+++ App.js
@@ -0,0 +1,46 @@
+// const heading = React.createElement(
+//   "h1",
+//   { id: "heading",  xyz: "abc" },
+//   "Hello World from React!"
+// );
+
+/***
+ *
+ * <div id="parent">
+ *      <div id="child">
+ *          <h1> I'm a h1 tag</h1>
+ *          <h2> I'm a h2 tag</h2>
+ *      </div>
+ *      <div id="child2">
+ *          <h1> I'm a h1 tag</h1>
+ *          <h2> I'm a h2 tag</h2>
+ *      </div>
+ * </div>
+ *
+ * now let's create a nested elements
+ */
+
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  [
+    React.createElement("div", { id: "child" }, [
+        React.createElement("h1", {}, "I'm a h1 tag"),
+        React.createElement("h2", {}, "I'm a h2 tag"),
+      ]),
+      React.createElement("div", { id: "child2" }, [
+        React.createElement("h1", {}, "I'm a h1 tag"),
+        React.createElement("h2", {}, "I'm a h2 tag"),
+      ])
+  ]
+);
+
+/**
+ * Now to avoid this nested unreadable code, we will use the JSX(Javascript Extension)
+ */
+
+console.log(parent);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent);
