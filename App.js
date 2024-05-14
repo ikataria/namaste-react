ab14f8425cfe836d89d5bee746b74279e26495d0
iKataria <17ktaria@gmail.com>
2024-05-14 15:37:12 -0400

    Episode 2 complete

--- App.js
+++ App.js
@@ -1,3 +1,6 @@
+import React from "react"; 
+import ReactDOM from "react-dom/client";
+
 // const heading = React.createElement(
 //   "h1",
 //   { id: "heading",  xyz: "abc" },
@@ -25,7 +28,7 @@ const parent = React.createElement(
   { id: "parent" },
   [
     React.createElement("div", { id: "child" }, [
-        React.createElement("h1", {}, "I'm a h1 tag"),
+        React.createElement("h1", {}, "This is Namaste React"),
         React.createElement("h2", {}, "I'm a h2 tag"),
       ]),
       React.createElement("div", { id: "child2" }, [
