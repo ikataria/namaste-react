1621f1b7ed0fc59f62bd37a849983fc914af1db5
iKataria <17ktaria@gmail.com>
2024-06-14 00:49:09 -0400

    restaurant card done

--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -6,15 +6,15 @@ const RestaurantCard = (props) => {
     resData?.info;
 
   return (
-    <div className="m-4 p-4 w-[250px] h-auto  rounded-lg bg-gray-200 hover:bg-gray-400">
-      <div className="flex items-center flex-col ">
+    <div className="m-4 p-4 w-[250px] h-auto  rounded-lg bg-gray-200 hover:bg-gray-400 lg:h-[300px]">
+      <div className="flex items-center  flex-col ">
         <img
           className="min-w-44 h-28 rounded-lg "
           alt="Food"
           src={CDN_URL + cloudinaryImageId}
         />
         <h3 className="font-bold pt-4 pb-1 text-md">{name}</h3>
-        <h4 className="font-normal text-xs">{cuisines.join(", ")}</h4>
+        <h4 className="font-normal text-xs lg:text-sm">{cuisines.join(", ")}</h4>
         <h4 className="font-medium pt-2 text-sm">
           ⭐{avgRating} {"   |  "} {sla.deliveryTime} minutes
         </h4>
